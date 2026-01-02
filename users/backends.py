4a5b9512b1437f9e2d795e24e2dcab78d8123883
Igor231321 <chernobay.i2112@gmail.com>
2026-01-02 21:55:21 +0200

    feat(auth): support custom kwargs in authentication backend

--- users/backends.py
+++ users/backends.py
@@ -3,9 +3,10 @@ from django.contrib.auth.backends import ModelBackend
 
 UserModel = get_user_model()
 
-
 class EmailAuthBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, ):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
         try:
             user = UserModel.objects.get(email=username)
             if user.check_password(password):
