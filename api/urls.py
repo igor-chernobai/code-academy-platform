3d5385da8d832f0f32ffebdfdd6e60e72678acc8
Igor231321 <chernobay.i2112@gmail.com>
2026-01-03 21:22:11 +0200

    feat: add JWT Token authentication

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 from . import views
 
@@ -15,4 +16,7 @@ urlpatterns = [
     path('course/<int:course_id>/lesson/<slug:slug>/', views.StudentLessonRetrieveAPIView.as_view()),
     path('course/<int:course_id>/', views.StudentLessonRetrieveAPIView.as_view()),
     path('auth-token/', obtain_auth_token),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 ]
