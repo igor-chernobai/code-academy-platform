ac8324c5b64f32465aba8a383cd4c33c5e2f547c
Igor Chernobay <chernobay.i2112@gmail.com>
2026-06-01 11:51:13 +0300

    chore: configure static files and Tailwind dev container

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-echo "Preparing media directory..."
-mkdir -p /usr/src/app/media
-chown -R nonroot:nonroot /usr/src/app/media
-
 echo "Applying migrations..."
 python manage.py migrate
 
 echo "Collecting static..."
 python manage.py collectstatic --noinput
 
-echo "Starting app as nonroot..."
-exec su -s /bin/sh nonroot -c "$*"
+echo "Starting app..."
+exec "$@"
