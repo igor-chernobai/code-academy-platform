6affb6bcdd622be021802960466ec00d047fd2fb
Igor Chernobai <chernobay.i2112@gmail.com>
2026-06-16 21:29:59 +0300

    feat: add POST /ask endpoint with echo response

--- ai_service/app/main.py
+++ ai_service/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from schemas import AnswerResponse, QuestionRequest
 
 app = FastAPI()
 
 
-@app.get('/health')
+@app.get("/health")
 def health():
-    return {'messege': 'OK'}
+    return {"messege": "OK"}
+
+
+@app.post("/ask")
+async def ask(question: QuestionRequest) -> AnswerResponse:
+    return AnswerResponse(answer="echo")
